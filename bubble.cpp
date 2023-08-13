5ac02a78acbc59f8054fbefb7e7feea6dc3849de
Jacob Zacarias <101657772+jacobzacarias@users.noreply.github.com>
2023-08-12 19:01:41 -0500

    Update bubble.cpp
    Added a check to handle the case where the array is empty. If the array is empty, an error message is printed to std::cerr, and the program returns with a non-zero exit code to indicate an error.

--- bubble.cpp
+++ bubble.cpp
@@ -1,12 +1,37 @@
 #include <iostream>
-#include <algorithm>
+#include <algorithm>  // Include the algorithm library for sorting
 
 int main() {
     int arr[] = {64, 34, 25, 12, 22, 11, 90};
-    std::sort(arr, arr + sizeof(arr) / sizeof(arr[0]));
-    std::cout << "Sorted array: ";
+
+    // Calculate the size of the array
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    // Check if the array is empty
+    if (size == 0) {
+        std::cerr << "Array is empty!" << std::endl;
+        return 1; // Indicate error with a non-zero exit code
+    }
+
+    // Sort the array in ascending order using the std::sort function
+    std::sort(arr, arr + size);
+
+    // Print the sorted array in ascending order
+    std::cout << "Sorted array in ascending order: ";
     for (int num : arr) {
         std::cout << num << " ";
     }
+    std::cout << std::endl;
+
+    // Sort the array in descending order
+    std::sort(arr, arr + size, std::greater<int>());
+
+    // Print the sorted array in descending order
+    std::cout << "Sorted array in descending order: ";
+    for (int num : arr) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
     return 0;
 }
