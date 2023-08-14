7b56e65b072da272dfcf986284f9ab3b47c25026
Jacob Zacarias <101657772+jacobzacarias@users.noreply.github.com>
2023-08-14 13:01:36 -0500

    Update code.cpp
    Added additional functions to main function.

--- code.cpp
+++ code.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 #include <unordered_map>
 
 std::string longestCommonSubsequence(const std::string& str1, const std::string& str2, int i, int j,
@@ -40,6 +41,30 @@ int main()
     std::string lcs = longestCommonSubsequence(str1, str2, str1.length(), str2.length(), memo);
 
     std::cout << "The Longest Common Subsequence is: " << lcs << std::endl;
+
+    // Additional functions
+
+    // 1. Get the length of the computed LCS
+    int lcsLength = lcs.length();
+    std::cout << "Length of LCS: " << lcsLength << std::endl;
+
+    // 2. Reverse the LCS string
+    std::string reversedLCS = lcs;
+    std::reverse(reversedLCS.begin(), reversedLCS.end());
+    std::cout << "Reversed LCS: " << reversedLCS << std::endl;
+
+    // 3. Count the number of 'A's in the LCS
+    int countA = std::count(lcs.begin(), lcs.end(), 'A');
+    std::cout << "Count of 'A's in LCS: " << countA << std::endl;
+
+    // 4. Convert LCS to uppercase
+    std::string uppercaseLCS = lcs;
+    std::transform(uppercaseLCS.begin(), uppercaseLCS.end(), uppercaseLCS.begin(), ::toupper);
+    std::cout << "Uppercase LCS: " << uppercaseLCS << std::endl;
+
+    // 5. Concatenate the two input strings with the LCS in between
+    std::string concatenated = str1 + " - " + lcs + " - " + str2;
+    std::cout << "Concatenated string: " << concatenated << std::endl;
 
     return 0;
 }
